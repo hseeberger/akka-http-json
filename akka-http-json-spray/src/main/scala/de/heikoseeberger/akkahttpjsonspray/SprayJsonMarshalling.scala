40492021896ffc0406f4c89082687e8ed1eb572f
Heiko Seeberger <mail@heikoseeberger.de>
2015-02-27 18:34:02 +0100

    Add SprayJsonMarshalling

---
+++ akka-http-json-spray/src/main/scala/de/heikoseeberger/akkahttpjsonspray/SprayJsonMarshalling.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.akkahttpjsonspray
+
+import akka.http.marshalling.{ PredefinedToEntityMarshallers, ToEntityMarshaller }
+import akka.http.model.MediaTypes
+import akka.http.unmarshalling.{ FromEntityUnmarshaller, PredefinedFromEntityUnmarshallers }
+import akka.stream.ActorFlowMaterializer
+import scala.concurrent.ExecutionContext
+import spray.json.{ JsonParser, JsonPrinter, PrettyPrinter, RootJsonReader, RootJsonWriter }
+
+object SprayJsonMarshalling extends SprayJsonMarshalling
+
+/** spray-json integration for Akka HTTP (un)marshalling. */
+trait SprayJsonMarshalling {
+
+  /** `FromEntityUnmarshaller` for `application/json` depending on a spray-json `RootJsonReader`. */
+  // TODO: Change ActorFlowMaterializer to FlowMaterializer once #16963 is fixed!
+  implicit def unmarshaller[A](implicit reader: RootJsonReader[A], ec: ExecutionContext, mat: ActorFlowMaterializer): FromEntityUnmarshaller[A] =
+    PredefinedFromEntityUnmarshallers
+      .stringUnmarshaller
+      .forContentTypes(MediaTypes.`application/json`)
+      .map(s => reader.read(JsonParser(s)))
+
+  /** `ToEntityMarshaller` to `application/json` depending on a spray-json `RootJsonWriter`. */
+  implicit def marshaller[A](implicit writer: RootJsonWriter[A], printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[A] =
+    PredefinedToEntityMarshallers
+      .stringMarshaller(MediaTypes.`application/json`)
+      .compose(printer)
+      .compose(writer.write)
+}
