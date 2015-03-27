8cbcdb1efe13b168d256c4535819a6bdc2901bb3
Heiko Seeberger <mail@heikoseeberger.de>
2015-03-27 18:00:39 +0100

    Prepare release 0.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -56,7 +56,7 @@ object Build extends AutoPlugin {
     ) ++
     // Git settings
     List(
-      GitPlugin.autoImport.git.baseVersion := "0.3.0"
+      GitPlugin.autoImport.git.baseVersion := "0.4.0"
     ) ++
     // Header settings
     List(
