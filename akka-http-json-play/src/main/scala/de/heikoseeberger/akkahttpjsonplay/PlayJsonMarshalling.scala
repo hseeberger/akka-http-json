f544ea87029a72683c7b260a837506710b1006bc
Heiko Seeberger <mail@heikoseeberger.de>
2015-04-19 07:49:05 +0200

    Improve handling of parse errors in akka-http-json-play (closes #11)

--- akka-http-json-play/src/main/scala/de/heikoseeberger/akkahttpjsonplay/PlayJsonMarshalling.scala
+++ akka-http-json-play/src/main/scala/de/heikoseeberger/akkahttpjsonplay/PlayJsonMarshalling.scala
@@ -20,7 +20,7 @@ import akka.http.marshalling.{ PredefinedToEntityMarshallers, ToEntityMarshaller
 import akka.http.model.MediaTypes
 import akka.http.unmarshalling.{ FromEntityUnmarshaller, PredefinedFromEntityUnmarshallers }
 import akka.stream.FlowMaterializer
-import play.api.libs.json.{ Json, Reads, Writes }
+import play.api.libs.json.{ JsError, Json, Reads, Writes }
 import scala.concurrent.ExecutionContext
 
 object PlayJsonMarshalling extends PlayJsonMarshalling
@@ -30,14 +30,12 @@ trait PlayJsonMarshalling {
 
   /** `FromEntityUnmarshaller` for `application/json` depending on a Play JSON `Reads`. */
   implicit def unmarshaller[A](implicit reads: Reads[A], ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[A] =
-    PredefinedFromEntityUnmarshallers
-      .stringUnmarshaller
+    PredefinedFromEntityUnmarshallers.stringUnmarshaller
       .forContentTypes(MediaTypes.`application/json`)
-      .map(s => reads.reads(Json.parse(s)).get)
+      .map(s => reads.reads(Json.parse(s)).recoverTotal(error => sys.error(JsError.toJson(error).toString)))
 
   /** `ToEntityMarshaller` to `application/json` depending on a Play JSON `Writes`. */
   implicit def marshaller[A](implicit writes: Writes[A]): ToEntityMarshaller[A] =
-    PredefinedToEntityMarshallers
-      .stringMarshaller(MediaTypes.`application/json`)
+    PredefinedToEntityMarshallers.stringMarshaller(MediaTypes.`application/json`)
       .compose(a => writes.writes(a).toString)
 }
