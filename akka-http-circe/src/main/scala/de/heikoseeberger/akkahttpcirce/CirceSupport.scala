958fd5c9fee0568b4f1fd09eb5abfc21075c69ae
Witi <witi83@users.noreply.github.com>
2016-04-15 08:14:40 +0200

    Optimize circe marshaller: (#61)

--- akka-http-circe/src/main/scala/de/heikoseeberger/akkahttpcirce/CirceSupport.scala
+++ akka-http-circe/src/main/scala/de/heikoseeberger/akkahttpcirce/CirceSupport.scala
@@ -17,10 +17,9 @@
 package de.heikoseeberger.akkahttpcirce
 
 import akka.http.scaladsl.marshalling.{ Marshaller, ToEntityMarshaller }
-import akka.http.scaladsl.model.{ HttpCharsets, MediaTypes }
+import akka.http.scaladsl.model.MediaTypes
 import akka.http.scaladsl.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
-import cats.data.Xor
-import io.circe.{ Decoder, Encoder, Json, Printer, jawn }
+import io.circe._
 
 /**
  * Automatic to and from JSON marshalling/unmarshalling using an in-scope *Circe* protocol.
@@ -47,30 +46,11 @@ trait CirceSupport {
    * @return unmarshaller for `A`
    */
   implicit def circeUnmarshaller[A](implicit decoder: Decoder[A]): FromEntityUnmarshaller[A] =
-    circeJsonUnmarshaller.map { json =>
-      decoder.decodeJson(json) match {
-        case Xor.Right(entity) => entity
-        case Xor.Left(e)       => throw e
-      }
-    }
-
-  /**
-   * HTTP entity => JSON
-   *
-   * @return unmarshaller for Circe Json
-   */
-  implicit def circeJsonUnmarshaller: FromEntityUnmarshaller[Json] =
     Unmarshaller
       .byteStringUnmarshaller
       .forContentTypes(MediaTypes.`application/json`)
       .mapWithCharset { (data, charset) =>
-        val input =
-          if (charset == HttpCharsets.`UTF-8`) data.utf8String
-          else data.decodeString(charset.nioCharset.name)
-        jawn.parse(input) match {
-          case Xor.Right(json) => json
-          case Xor.Left(e)     => throw e
-        }
+        jawn.decode(data.decodeString(charset.nioCharset.name)) valueOr (throw _)
       }
 
   implicit def circeToEntityMarshallerConverter[A](encoder: Encoder[A])(implicit printer: Json => String = Printer.noSpaces.pretty): ToEntityMarshaller[A] =
