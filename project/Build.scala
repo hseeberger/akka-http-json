ef1440316c0cad1be2b0246986ad8db432628591
Heiko Seeberger <mail@heikoseeberger.de>
2016-11-13 12:07:39 +0100

    Replace pomExtra with core settings

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,6 @@ import bintray.BintrayPlugin
 import bintray.BintrayPlugin.autoImport._
 import com.typesafe.sbt.GitPlugin
 import com.typesafe.sbt.GitPlugin.autoImport._
-import com.typesafe.sbt.SbtPgp
 import de.heikoseeberger.sbtheader.HeaderPlugin
 import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport._
 import de.heikoseeberger.sbtheader.license.Apache2_0
@@ -24,8 +23,10 @@ object Build extends AutoPlugin {
     Vector(
       // Core settings
       organization := "de.heikoseeberger",
-      licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
-      mappings.in(Compile, packageBin) += baseDirectory.in(ThisBuild).value / "LICENSE" -> "LICENSE",
+      licenses += ("Apache-2.0",
+                   url("http://www.apache.org/licenses/LICENSE-2.0")),
+      mappings.in(Compile, packageBin) +=
+        baseDirectory.in(ThisBuild).value / "LICENSE" -> "LICENSE",
       scalaVersion := Version.Scala,
       crossScalaVersions := Vector(scalaVersion.value, "2.11.8"),
       scalacOptions ++= Vector(
@@ -35,26 +36,25 @@ object Build extends AutoPlugin {
         "-target:jvm-1.8",
         "-encoding", "UTF-8"
       ),
-      unmanagedSourceDirectories.in(Compile) := Vector(scalaSource.in(Compile).value),
-      unmanagedSourceDirectories.in(Test) := Vector(scalaSource.in(Test).value),
+      unmanagedSourceDirectories.in(Compile) :=
+        Vector(scalaSource.in(Compile).value),
+      unmanagedSourceDirectories.in(Test) :=
+        Vector(scalaSource.in(Test).value),
+
+      // POM settings for Sonatype
       homepage := Some(url("https://github.com/hseeberger/akka-http-json")),
+      scmInfo := Some(ScmInfo(url("https://github.com/hseeberger/akka-http-json"),
+                                  "git@github.com:hseeberger/akka-http-json.git")),
+      developers += Developer("hseeberger",
+                              "Heiko Seeberger",
+                              "mail@heikoseeberger.de",
+                              url("https://github.com/hseeberger")),
       pomIncludeRepository := (_ => false),
-      pomExtra := <scm>
-                    <url>https://github.com/hseeberger/akka-http-json</url>
-                    <connection>scm:git:git@github.com:hseeberger/akka-http-json.git</connection>
-                  </scm>
-                  <developers>
-                    <developer>
-                      <id>hseeberger</id>
-                      <name>Heiko Seeberger</name>
-                      <url>http://heikoseeberger.de</url>
-                    </developer>
-                  </developers>,
 
       // scalafmt settings
       formatSbtFiles := false,
       scalafmtConfig := Some(baseDirectory.in(ThisBuild).value / ".scalafmt.conf"),
-      ivyScala       := ivyScala.value.map(_.copy(overrideScalaVersion = sbtPlugin.value)), // TODO Remove once this workaround no longer needed (https://github.com/sbt/sbt/issues/2786)!
+      ivyScala := ivyScala.value.map(_.copy(overrideScalaVersion = sbtPlugin.value)), // TODO Remove once this workaround no longer needed (https://github.com/sbt/sbt/issues/2786)!
 
       // Git settings
       git.useGitDescribe := true,
