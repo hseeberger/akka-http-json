76f6c6f061cfa792ba8bfe306f7fab35e40677ae
Heiko Seeberger <mail@heikoseeberger.de>
2017-01-25 09:02:23 +0000

    Upgrade various library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Version {
-  final val Akka         = "2.4.14"
-  final val AkkaHttp     = "10.0.0"
+  final val Akka         = "2.4.16"
+  final val AkkaHttp     = "10.0.2"
   final val Argonaut     = "6.2-RC2"
   final val Circe        = "0.7.0"
-  final val JacksonScala = "2.8.4"
+  final val JacksonScala = "2.8.6"
   final val Json4s       = "3.5.0"
   final val Play         = "2.6.0-M1"
   final val Scala        = "2.12.1"
