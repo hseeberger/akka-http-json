b3c4eb4c23886c5efa31886b6a64179d514d3022
Denis Borisenko <dborisenko@gmail.com>
2017-08-01 23:25:23 +0200

    Added support for avro4s json serialization/deserialization with avro schemes

---
+++ akka-http-avro4s/src/test/scala/de/heikoseeberger/akkahttpavro4s/AvroSupportSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.akkahttpavro4s
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.ContentTypes.`application/json`
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshaller.UnsupportedContentTypeException
+import akka.http.scaladsl.unmarshalling.{ Unmarshal, Unmarshaller }
+import akka.stream.ActorMaterializer
+import com.sksamuel.avro4s.{ FromRecord, SchemaFor, ToRecord }
+import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, Matchers }
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+object AvroSupportSpec {
+
+  final case class Foo(bar: String) {
+    require(bar == "bar", "bar must be 'bar'!")
+  }
+}
+
+final class AvroSupportSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
+
+  import AvroSupportSpec._
+
+  private implicit val system     = ActorSystem()
+  private implicit val mat        = ActorMaterializer()
+  private implicit val schemaFor  = SchemaFor[Foo]
+  private implicit val toRecord   = ToRecord[Foo]
+  private implicit val fromRecord = FromRecord[Foo]
+
+  "AvroSupport" should {
+    "enable marshalling and unmarshalling objects for generic derivation" in {
+      import AvroSupport._
+
+      val foo = Foo("bar")
+      Marshal(foo)
+        .to[RequestEntity]
+        .flatMap(Unmarshal(_).to[Foo])
+        .map(_ shouldBe foo)
+    }
+
+    "provide proper error messages for requirement errors" in {
+      import AvroSupport._
+
+      val entity = HttpEntity(MediaTypes.`application/json`, """{ "bar": "baz" }""")
+      Unmarshal(entity)
+        .to[Foo]
+        .failed
+        .map(_ should have message "requirement failed: bar must be 'bar'!")
+    }
+
+    "fail with NoContentException when unmarshalling empty entities" in {
+      import AvroSupport._
+
+      val entity = HttpEntity.empty(`application/json`)
+      Unmarshal(entity)
+        .to[Foo]
+        .failed
+        .map(_ shouldBe Unmarshaller.NoContentException)
+    }
+
+    "fail with UnsupportedContentTypeException when Content-Type is not `application/json`" in {
+      import AvroSupport._
+
+      val entity = HttpEntity("""{ "bar": "bar" }""")
+      Unmarshal(entity)
+        .to[Foo]
+        .failed
+        .map(_ shouldBe UnsupportedContentTypeException(`application/json`))
+    }
+
+    "allow unmarshalling with passed in Content-Types" in {
+      val foo = Foo("bar")
+      val `application/json-home` =
+        MediaType.applicationWithFixedCharset("json-home", HttpCharsets.`UTF-8`, "json-home")
+
+      final object CustomAvroSupport extends AvroSupport {
+        override def unmarshallerContentTypes = List(`application/json`, `application/json-home`)
+      }
+      import CustomAvroSupport._
+
+      val entity = HttpEntity(`application/json-home`, """{ "bar": "bar" }""")
+      Unmarshal(entity).to[Foo].map(_ shouldBe foo)
+    }
+  }
+
+  override protected def afterAll() = {
+    Await.ready(system.terminate(), 42.seconds)
+    super.afterAll()
+  }
+}
