e68596d92a126580c1f9c3c07c93ff815a28e46b
Passarinho4 <passarinho4@gmail.com>
2018-06-08 22:38:17 +0200

    AVSystem commons GenCodec support added. It is powerful typeclass based serialization framework with native support for Json Serialization. I believe it could be very useful in many cases.

---
+++ akka-http-avsystem-gencodec/src/main/scala/de/heikoseeberger/akkahttpavsystemgencodec/GenCodecSupport.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.akkahttpavsystemgencodec
+
+import akka.http.scaladsl.marshalling.{ Marshaller, ToEntityMarshaller }
+import akka.http.scaladsl.model.ContentTypeRange
+import akka.http.scaladsl.model.MediaType
+import akka.http.scaladsl.model.MediaTypes.`application/json`
+import akka.http.scaladsl.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
+import akka.util.ByteString
+import com.avsystem.commons.serialization.GenCodec
+import com.avsystem.commons.serialization.json.{ JsonStringInput, JsonStringOutput }
+
+import scala.collection.immutable.Seq
+
+object GenCodecSupport extends GenCodecSupport {}
+
+trait GenCodecSupport {
+
+  def unmarshallerContentTypes: Seq[ContentTypeRange] =
+    mediaTypes.map(ContentTypeRange.apply)
+
+  def mediaTypes: Seq[MediaType.WithFixedCharset] =
+    List(`application/json`)
+
+  private val jsonStringUnmarshaller =
+    Unmarshaller.byteStringUnmarshaller
+      .forContentTypes(unmarshallerContentTypes: _*)
+      .mapWithCharset {
+        case (ByteString.empty, _) => throw Unmarshaller.NoContentException
+        case (data, charset)       => data.decodeString(charset.nioCharset.name)
+      }
+
+  private val jsonStringMarshaller =
+    Marshaller.oneOf(mediaTypes: _*)(Marshaller.stringMarshaller)
+
+  /**
+    * HTTP entity => `A`
+    *
+    * @tparam A type to decode
+    * @return unmarshaller for `A`
+    */
+  implicit def unmarshaller[A: GenCodec]: FromEntityUnmarshaller[A] =
+    jsonStringUnmarshaller.map(data => JsonStringInput.read[A](data))
+
+  /**
+    * `A` => HTTP entity
+    *
+    * @tparam A type to encode
+    * @return marshaller for any `A` value
+    */
+  implicit def marshaller[A: GenCodec]: ToEntityMarshaller[A] =
+    jsonStringMarshaller.compose(JsonStringOutput.write(_))
+}
